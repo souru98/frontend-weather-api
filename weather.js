9ee9a153b1f27ffd35e48234ea568882424cabe1
sourabh kulkarni <kulkarnisourabh1998@gmail.com>
2020-06-13 01:43:44 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,18 +2,18 @@
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
-var button = document.querySelector('.submit');
-// var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
 
-button.addEventListener('click', function(name) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=721f1bf7346d1594961b9f09569d150a')
-        .then(response => response.json())
- => {
-            var tempValue = data['main']['temp'];
-            var nameValue = data['name'];
-            var descValue = data['weather'][0]['description'];
+
+
+
+
+
+
+
+
+
+
  
 
 
